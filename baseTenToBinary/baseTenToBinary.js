605181ac034a18f4b8356382ae642ba094029415
claire <cdersky@gmail.com>
2016-07-29 19:31:15 -0700

    add baseTenToBinary

---
+++ baseTenToBinary/baseTenToBinary.js
@@ -0,0 +1,18 @@
+//Create a function that converts a base ten number to a binary. If the user uses an invalid number, return false;
+
+//Examples: 
+/// (5) should return 101.
+/// baseTenToBinary(3r84) should return false.
+
+function baseTenToBinary() {
+ //your code here
+}
+
+
+//Test Cases
+
+// baseTenToBinary(169) should return 10101001.
+// baseTenToBinary(1) should return 1.
+// baseTenToBinary(100000) should return 32.
+// baseTenToBinary(13) should return 1101.
+// baseTenToBinary(2*123) should return false.
