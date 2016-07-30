7bb8e563bd2a915233dfd6144a48714e2b1d451a
claire <cdersky@gmail.com>
2016-07-29 20:41:27 -0700

    add dayOfTheWeek

---
+++ dayOfTheWeek/dayOfTheWeek.js
@@ -0,0 +1,28 @@
+// Day of the Week
+// Use the Zeller's congruence to calculate the day of the week you will turn 31 years old. 
+
+// The input should be a string of the date that takes the form of mm-dd-yyyy.
+
+// The output should be a string of the day of the date.
+
+// Note: Zeller's congruence is valid to calculate past and future dates.
+
+// Hints
+// https://en.wikipedia.org/wiki/Zeller%27s_congruence
+// http://www.homeworkforyou.com/static/uploadedfiles/User_2781732014Zeller.pdf
+
+function dayOfTheWeek(date) {
+  //your code here
+}
+
+dayOfTheWeek("04-17-1924"); // Thursday
+
+// TEST CASES
+
+// dayOfTheWeek("04-17-2101") should return "Sunday"
+// dayOfTheWeek("11-19-1863") should return "Thursday"
+// dayOfTheWeek("01-01-1901") should return "Tuesday"
+// dayOfTheWeek("05-06-2005") should return "Friday"
+
+
+// Bonus, integrate this algorithm with the Friendly Date Ranges Algorithm
