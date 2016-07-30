bc2eed2592fa4475442b7eec96b83c24dd1d2637
claire <cdersky@gmail.com>
2016-07-29 19:26:07 -0700

    add binaryToBaseTen

---
+++ binaryToBaseTen/binaryToBaseTen.js
@@ -0,0 +1,18 @@
+//Create a function that converts a binary number to a base ten number. If the user uses a non-binary number, return the boolean false;
+
+//Examples: 
+/// binaryToBaseTen(100101) should return 37.
+/// binaryToBaseTen(100201) should return false.
+
+function binaryToBaseTen() {
+ //your code here
+}
+
+
+//Test Cases
+
+// binaryToBaseTen(10) should return 2.
+// binaryToBaseTen(010) should return 2.
+// binaryToBaseTen(100000) should return 32.
+// binaryToBaseTen(1111111111111111101) should return 524285.
+// binaryToBaseTen(23482937235) should return false.
